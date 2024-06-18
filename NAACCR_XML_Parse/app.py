e3fabdcd41044d97af771635c3f2f0bfa0e43817
bphon <brian.phong@albertahealthservices.ca>
2024-06-18 10:13:11 -0600

    added better mock data to be more like data from v230 xml version, changed generate_sample_xml function to output necessary xml format

--- NAACCR_XML_Parse/app.py
+++ NAACCR_XML_Parse/app.py
@@ -40,7 +40,7 @@ class App:
                     logging.error(error)
 
             # Export data to tab-delimited text (optional)
-            Repository().ExportToTabDelimitedText()  
+            Repository().ExportToTabDelimitedText()
             logging.info('*** Stop ***')
         except Exception as e:
             logging.exception("Exception occurred")
@@ -82,21 +82,96 @@ class App:
         return is_valid, xsd_schema.error_log
 
     def mock_oracle_data(self):
-        # Simulate the data retrieved from Oracle
+        # Simulate the data retrieved from Oracle with multiple tumors per patient
         return [
             {
-                "patientIdNumber": "123456",
-                "medicalRecordNumber": "MRN001",
-                "tumorRecordNumber": "TRN001",
-                "primarySite": "C509",
-                "histology": "8500/3"
-            },
-            {
-                "patientIdNumber": "123457",
-                "medicalRecordNumber": "MRN002",
-                "tumorRecordNumber": "TRN002",
-                "primarySite": "C504",
-                "histology": "8520/3"
+                "patientIdNumber": "00100001",
+                "birthplaceCountry": "ZZU",
+                "sex": "2",
+                "spanishHispanicOrigin": "9",
+                "dateOfBirth": "19750102",
+                "placeOfDeath": "997",
+                "raceCodingSysCurrent": "6",
+                "computedEthnicity": "0",
+                "vitalStatus": "1",
+                "race5": "99",
+                "raceCodingSysOriginal": "9",
+                "birthplace": "999",
+                "race1": "99",
+                "race2": "99",
+                "race3": "99",
+                "dateOfLastContact": "20240401",
+                "race4": "99",
+                "birthplaceState": "ZZ",
+                "causeOfDeath": "0000",
+                "icdRevisionNumber": "0",
+                "tumors": [
+                    {
+                        "tumorRecordNumber": "01",
+                        "primarySite": "C505",
+                        "histologicTypeIcdO3": "8500",
+                        "metsAtDxBrain": "0",
+                        "lymphVascularInvasion": "1",
+                        "ajccTnmClinN": "cN2a",
+                        "regionalNodesPositive": "03",
+                        "ajccTnmClinM": "cM0",
+                        "siteCodingSysCurrent": "5",
+                        "ajccTnmClinT": "cT4a",
+                        "oncotypeDxRecurrenceScoreInvasiv": "XX9",
+                        "morphCodingSysOriginl": "7",
+                        "laterality": "2",
+                        "primaryPayerAtDx": "10",
+                        "rxSummHormone": "99",
+                        "ambiguousTerminologyDx": "0",
+                        "rxSummChemo": "99",
+                        "addrAtDxState": "AB",
+                        "icdO3ConversionFlag": "0",
+                        "rxSummBrm": "99",
+                        "gradeClinical": "3",
+                        "her2OverallSummary": "0",
+                        "siteCodingSysOriginal": "5",
+                        "tnmEditionNumber": "08",
+                        "ajccId": "48.2",
+                        "typeOfReportingSource": "1",
+                        "metsAtDxBone": "0",
+                        "metsAtDxDistantLn": "0",
+                        "schemaId": "00480",
+                        "multiplicityCounter": "01",
+                        "ajccTnmClinNSuffix": "(f)",
+                        "ajccTnmClinStageGroup": "3C",
+                        "addrAtDxPostalCode": "T0O0O0",
+                        "metsAtDxLung": "0",
+                        "countyAtDxGeocode2020": "998",
+                        "countyAtDx": "000",
+                        "ajccTnmPathStageGroup": "3A",
+                        "estrogenReceptorSummary": "0",
+                        "behaviorCodeIcdO3": "3",
+                        "gradePathological": "3",
+                        "reasonForNoSurgery": "1",
+                        "multTumRptAsOnePrim": "88",
+                        "ajccTnmPathT": "pT2",
+                        "ageAtDiagnosis": "087",
+                        "sequenceNumberCentral": "00",
+                        "dateOfDiagnosis": "20210102",
+                        "rxSummRadiation": "9",
+                        "morphCodingSysCurrent": "7",
+                        "regionalNodesExamined": "18",
+                        "metsAtDxOther": "0",
+                        "tumorSizeClinical": "040",
+                        "rxSummSurgPrimSite": "99",
+                        "followUpSourceCentral": "29",
+                        "rxSummScopeRegLnSur": "9",
+                        "followUpSource": "8",
+                        "diagnosticConfirmation": "1",
+                        "progesteroneRecepSummary": "0",
+                        "tumorSizePathologic": "043",
+                        "her2IshSummary": "8",
+                        "ajccTnmPathM": "cM0",
+                        "ajccTnmPathN": "pN1a",
+                        "metsAtDxLiver": "0",
+                        "rxSummOther": "9"
+                    }
+                ]
             }
         ]
 
@@ -108,36 +183,47 @@ class App:
                              recordType="I",
                              specificationVersion="1.6")
 
+        # Add global items
+        global_items = [
+            {"naaccrId": "registryType", "value": "1"},
+            {"naaccrId": "recordType", "value": "I"},
+            {"naaccrId": "naaccrRecordVersion", "value": "230"},
+            {"naaccrId": "registryId", "value": "0022004800"}
+        ]
+        for item in global_items:
+            item_element = etree.SubElement(root, "Item", naaccrId=item["naaccrId"])
+            item_element.text = item["value"]
+
         # Use the mock Oracle data to generate XML
         data = self.mock_oracle_data()
 
         # Parse the NAACCR dictionary to get patient and tumor item mappings
         dictionary_path = os.path.abspath(os.path.join(os.getcwd(), "NAACCR_XML_Parse", "naaccr-dictionary-230.xml"))
+        logging.info(f"Dictionary path: {dictionary_path}")  # Add this line to check the dictionary path
         patient_items, tumor_items = self.parse_naaccr_dictionary(dictionary_path)
 
         for patient_data in data:
             # Create a Patient element
             patient_element = etree.SubElement(root, "Patient")
-            
+
             # Adding Patient Items
             for naaccr_id, value in patient_data.items():
                 if naaccr_id in patient_items:
                     item_element = etree.SubElement(patient_element, "Item", naaccrId=naaccr_id)
                     item_element.text = value
-            
-            # Create a Tumor element
-            tumor_element = etree.SubElement(patient_element, "Tumor")
 
-            # Adding Tumor Items
-            for naaccr_id, value in patient_data.items():
-                if naaccr_id in tumor_items:
-                    item_element = etree.SubElement(tumor_element, "Item", naaccrId=naaccr_id)
-                    item_element.text = value
+            # Adding Tumor elements
+            for tumor_data in patient_data["tumors"]:
+                tumor_element = etree.SubElement(patient_element, "Tumor")
+                for naaccr_id, value in tumor_data.items():
+                    if naaccr_id in tumor_items:
+                        item_element = etree.SubElement(tumor_element, "Item", naaccrId=naaccr_id)
+                        item_element.text = value
 
         # Convert the XML tree to a string
         xml_data = etree.tostring(root, pretty_print=True, xml_declaration=True, encoding="UTF-8")
         return xml_data
-    
+
     def parse_naaccr_dictionary(self, dictionary_path):
         # Parse the NAACCR dictionary XML
         tree = etree.parse(dictionary_path)
@@ -153,7 +239,7 @@ class App:
                 patient_items.add(naaccr_id)
             elif parent_tag == "Tumor":
                 tumor_items.add(naaccr_id)
-        
+
         return patient_items, tumor_items
 
     def save_xml(self, xml_data, filename):
